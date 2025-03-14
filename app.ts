680784e7710475e58195dc11cc7eb52278de608f
JanKulesza <kulesz.jasiek@gmail.com>
2025-03-14 15:47:28 +0100

    Use express to handle requests

--- app.ts
+++ app.ts
@@ -1,18 +1,56 @@
-import { createServer } from "http";
+import { configDotenv } from "dotenv";
+import express from "express";
+import {
+  courseInputSchema,
+  type CourseInputType,
+} from "./utils/schema/index.ts";
 
-const server = createServer((req, res) => {
-  console.log(req.url);
+configDotenv();
 
-  if (req.url === "/") {
-    res.write("Hello World!");
-    res.end();
-  }
-  if (req.url === "/courses") {
-    res.write(JSON.stringify([1, 2, 3]));
-    res.end();
-  }
+const app = express();
+
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(3000, () => {
-  console.log("Listening on port 8080...");
+app.get("/api/courses", (req, res) => {
+  res.json(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const { id } = req.params;
+  const { sortBy } = req.query;
+
+  const course = courses.find((c) => c.id === +id);
+  if (!course) res.status(404).json("Course not found");
+
+  res.json({ ...course, sortBy });
+});
+
+app.post("/api/courses", (req, res) => {
+  const body = req.body;
+
+  const validation = courseInputSchema.safeParse(body);
+  if (!validation.success) {
+    res.status(400).send(validation.error.errors.map((e) => e.message));
+    return;
+  }
+
+  const { name } = req.body as CourseInputType;
+  const course = { id: courses.length + 1, name };
+  courses.push(course);
+  res.send(course);
+});
+
+const PORT = process.env.PORT ?? 8080;
+app.listen(PORT, () => {
+  console.log(`Started server at http://localhost:${PORT}`);
 });
---
+++ utils/schema/index.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const courseInputSchema = z.object({
+  name: z
+    .string({ required_error: "Name is required." })
+    .min(3, "Name is too short."),
+});
+
+export type CourseInputType = z.infer<typeof courseInputSchema>;
