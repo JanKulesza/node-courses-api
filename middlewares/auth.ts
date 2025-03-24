81807c225320c7d6ecb0639333bc7e31305a6543
JanKulesza <kulesz.jasiek@gmail.com>
2025-03-24 12:04:19 +0100

    Fix jwt imports

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response, type NextFunction } from "express";
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
   const token = req.header("Authorization");
